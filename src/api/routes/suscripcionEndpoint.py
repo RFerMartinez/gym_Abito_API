3052381e55f7b3a379ecda2c8d83ebbeba0e027d
Fer Martínez <ricardofernandomartinez0@gmail.com>
2025-08-01 00:22:30 -0300

    add methods get, put, delete for subs

--- src/api/routes/suscripcionEndpoint.py
+++ src/api/routes/suscripcionEndpoint.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from asyncpg import Connection
-from services.suscripcionSerives import create
-from schemas.suscripcionSchema import SuscripcionCreate, SuscripcionCreateResponse
+from services.suscripcionSerives import create, get_all, update_suscription_price, delete_subscription
+from schemas.suscripcionSchema import (
+    SuscripcionCreate,
+    SuscripcionCreateResponse, 
+    SuscripcionBase,
+    SuscripcionUpdatePrice)
 from typing import List
 from core.session import get_db
 
 router = APIRouter(prefix="/suscripciones", tags=["suscripciones"])
 
+# CREAR SSUCRIPCIÓN
 @router.post("/", response_model=SuscripcionCreateResponse, status_code=201)
 async def crear_suscripcion(
     suscripcion: SuscripcionCreate,
@@ -14,3 +19,20 @@ async def crear_suscripcion(
 ):
     return await create(con, suscripcion)
 
+# OBTENER TODAS LAS SUSCRIPCIONES
+@router.get("/", response_model=List[SuscripcionBase])
+async def listar_suscripciones(con: Connection = Depends(get_db)):
+    return await get_all(con)
+
+# ACTUALIZAR PRECIO DE UNA SUSCRIPCION
+@router.put("/{nameSub}")
+async def actualizar_precio_suscripcion(
+    sub: SuscripcionUpdatePrice,
+    con: Connection = Depends(get_db)
+):
+    return await update_suscription_price(con=con, subs_data=sub)
+
+# ELIMINAR UNA SUSCRIPCION
+@router.delete("/{nameSub}")
+async def eliminar_suscripcion(nombreSuscripcion: str, con: Connection = Depends(get_db)):
+    return await delete_subscription(con=con, nombre=nombreSuscripcion)
--- src/services/suscripcionSerives.py
+++ src/services/suscripcionSerives.py
@@ -4,9 +4,13 @@ from typing import List, Optional
 from fastapi import HTTPException
 from  asyncpg import Connection
 
-from schemas.suscripcionSchema import SuscripcionCreate, SuscripcionCreateResponse
+from schemas.suscripcionSchema import (
+    SuscripcionCreate,
+    SuscripcionCreateResponse,
+    SuscripcionBase,
+    SuscripcionUpdatePrice)
 
-# SERVICE para crear una suscripcion nueba
+# SERVICE para crear una suscripcion nueva
 async def create(
         con: Connection,
         suscripcion_data: SuscripcionCreate
@@ -27,4 +31,55 @@ async def create(
         if "duplicate key" in str(e):
             raise HTTPException(status_code=400, detail="La suscripción ya existe")
         raise HTTPException(status_code=500, detail="Error al crear suscripción")
+
+# SERVICE para obtener todas las suscripciones
+async def get_all(con: Connection) -> List[SuscripcionBase]:
+    try:
+        query = """
+            SELECT "nombreSuscripcion", precio
+            FROM "Suscripcion";
+        """
+        suscripciones = await con.fetch(query)
+        return [SuscripcionBase(**elem)for elem in suscripciones]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error al obtener suscripciones")
+
+# SERVICE para actualizar el precio de una suscripcion
+async def update_suscription_price(
+        con: Connection,
+        subs_data: SuscripcionUpdatePrice
+) -> SuscripcionBase:
+    try:
+        query = """
+            UPDATE "Suscripcion"
+            SET precio = $1
+            WHERE "nombreSuscripcion" = $2
+            RETURNING "nombreSuscripcion", precio;
+        """
+        suscripcion = await con.fetchrow(
+            query,
+            subs_data.precio,
+            subs_data.nombreSuscripcion)
+        if not suscripcion:
+            raise HTTPException(status_code=404, detail="Suscripción no encontrada")
+        return SuscripcionBase(**suscripcion)
+    except Exception:
+        raise HTTPException(status_code=500, detail="Error al actualizar suscripcion")
+
+# SERVICE para eliminar una suscripción
+async def delete_subscription(con: Connection, nombre: str) -> dict:
+    try:
+        query = """
+            DELETE FROM "Suscripcion"
+            WHERE "nombreSuscripcion" = $1
+            RETURNING "nombreSuscripcion";
+        """
+        res = await con.fetchrow(query, nombre)
+        if not res:
+            raise HTTPException(status_code=404, detail="Suscripción no encontrada")
+        return {
+            "message": "Suscripción eliminada correctamente"
+        }
+    except Exception:
+        raise HTTPException(status_code=500, detail="Error al elminar suscripción")
 
