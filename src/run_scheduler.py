ebcdc9c654249409b2e83f14902e281a4e9df565
RFerMartinez <ricardofernandomartinez0@gmail.com>
2026-02-12 15:59:41 -0300

    casi casi se termina

--- src/run_scheduler.py
+++ src/run_scheduler.py
@@ -7,6 +7,7 @@ from core.session import connect_to_db, close_db_connection, get_db
 # Importamos la función de generación de cuotas
 # Asegúrate de que la ruta sea correcta según tu estructura
 from services.cuotaServices import generar_cuotas_masivas_mensuales 
+from services.facturacionServices import procesar_cierre_automatico
 
 # Configuración de Logging
 logging.basicConfig(
@@ -33,6 +34,17 @@ async def tarea_generar_cuotas():
     except Exception as e:
         logging.error(f"[Scheduler] Error generando cuotas: {e}")
 
+async def tarea_cierre_facturacion():
+    """Se ejecuta el día 1 y 15 para cerrar la facturación."""
+    logging.info("[Scheduler] Iniciando cierre de facturación automático...")
+    try:
+        async for db in get_db(): 
+            await procesar_cierre_automatico(db)
+            logging.info("[Scheduler] Cierre de facturación finalizado con éxito.")
+            break 
+    except Exception as e:
+        logging.error(f"[Scheduler] Error en cierre de facturación: {e}")
+
 async def main():
     # 1. Iniciar conexión a DB
     await connect_to_db()
@@ -40,11 +52,18 @@ async def main():
     # 2. Configurar Scheduler
     scheduler = AsyncIOScheduler()
     
-    # Tarea: Generar Cuotas (Día 12 a las 14:45)
+    # Tarea: Generar Cuotas
     scheduler.add_job(
         tarea_generar_cuotas, 
-        CronTrigger(day=12, hour=15, minute=17),
+        CronTrigger(day=6, hour=0, minute=0),
         id="generacion_cuotas_mensual"
+    )
+
+    # Tarea: Generar Cierre quincenal (Dias 1 y 15 a las 23:30)
+    scheduler.add_job(
+        tarea_cierre_facturacion,
+        CronTrigger(day='1,15', hour=23, minute=30), # Días 1 y 15 a las 23:30
+        id="cierre_facturacion_auto"
     )
     
     # 3. Iniciar
